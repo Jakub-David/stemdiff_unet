c5db8a3211b6de650642de9693684c004586da0e
Jakub David <jakubdavid64@gmail.com>
2026-07-13 14:17:43 +0200

    update dependencies

--- mean_bkgp.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-sigmas = [1.5, 2.5, 2, 2, 2.5]
-thrs = [6, 1, 6, 6, 1]
-area_sizes = [7, 3, 5, 5, 3]
-
-print("sigma:", np.mean(sigmas))
-print("thr:", np.mean(thrs))
-print("area_size:", np.mean(area_sizes))
